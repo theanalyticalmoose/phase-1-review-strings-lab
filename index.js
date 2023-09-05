77e019acbf2b20cfa0e5718990222b921900576e
Rachael <rachael.brady@theanalyticalmoose.com>
2023-09-04 17:52:12 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Declare the variable 'currentUser' and set its value to a name.
+const currentUser = "John";
+
+// Create the variable 'welcomeMessage' containing "Welcome to Flatbook, " + the value of 'currentUser'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create the variable 'excitedWelcomeMessage' containing the uppercase version of 'welcomeMessage'
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create the variable 'shortGreeting' containing "Welcome, " + the first letter of 'currentUser' + '!'
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
